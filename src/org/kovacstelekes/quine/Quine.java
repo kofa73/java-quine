c7560ec88bbb7f4725bff9763b303a44240db350
Kofa <kofa@kovacs-telekes.org>
2023-02-03 15:18:04 +0100

    Further simplification

--- src/org/kovacstelekes/quine/Quine.java
+++ src/org/kovacstelekes/quine/Quine.java
@@ -1,22 +1,21 @@
 package org.kovacstelekes.quine;
 import static java.lang.String.valueOf;
 import static java.util.Arrays.stream;
-import java.util.stream.Collectors;
+import static java.util.stream.Collectors.joining;
 public class Quine {
 public static void main(String[] args) {
 String[] src = {
 "package org.kovacstelekes.quine;",
 "import static java.lang.String.valueOf;",
 "import static java.util.Arrays.stream;",
-"import java.util.stream.Collectors;",
+"import static java.util.stream.Collectors.joining;",
 "public class Quine {",
 "public static void main(String[] args) {",
 "String[] src = {%s};",
 "var qm = valueOf((char) 34);",
 "var comma = valueOf((char) 44);",
 "var nl = System.lineSeparator();",
-"var blank = valueOf(new char[0]);",
-"var allSrc = nl + stream(src).map(s -> qm + s + qm).collect(Collectors.joining(comma  + nl, blank, nl));",
+"var allSrc = stream(src).map(s -> qm + s + qm).collect(joining(comma  + nl, nl, nl));",
 "stream(src).map(line -> line.formatted(allSrc)).forEach(System.out::println);",
 "}",
 "}"
@@ -24,8 +23,7 @@ String[] src = {
 var qm = valueOf((char) 34);
 var comma = valueOf((char) 44);
 var nl = System.lineSeparator();
-var blank = valueOf(new char[0]);
-var allSrc = nl + stream(src).map(s -> qm + s + qm).collect(Collectors.joining(comma  + nl, blank, nl));
+var allSrc = stream(src).map(s -> qm + s + qm).collect(joining(comma  + nl, nl, nl));
 stream(src).map(line -> line.formatted(allSrc)).forEach(System.out::println);
 }
 }
